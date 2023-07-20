065198318a1a9a7236ed28d2eb95a3e977a9a9e4
sevvandi <sevvandi.kandanaarachchi@gmail.com>
2023-07-20 16:44:00 +1000

    add autoplot heatmaps_crm

---
+++ R/airt-package.R
@@ -0,0 +1,11 @@
+#' @importFrom ggplot2 aes coord_fixed facet_wrap geom_path geom_point ggplot ggtitle geom_tile
+#' @importFrom ggplot2 theme_bw xlab ylab geom_line theme geom_raster scale_fill_viridis_c
+#' @importFrom ggplot2 autoplot
+#' @export
+ggplot2::autoplot
+NULL
+
+#' @docType package
+#' @aliases NULL airt-package
+#' @keywords internal
+"_PACKAGE"
--- R/utils.R
+++ R/utils.R
@@ -9,11 +9,11 @@
 #' @examples
 #' data(classification_poly)
 #' mod <- pirtmodel(classification_poly)
-#' dat <- prepare_for_plots_poly(mod$model)
+#' dat <- tracelines_poly(mod$model)
 #' head(dat)
 #'
 #' @export
-prepare_for_plots_poly <- function(mod){
+tracelines_poly <- function(mod){
   # mod is the trained model
   num_algos <- dim(mod@Data$data)[2]
   names_algos <- colnames(mod@Data$data)
@@ -36,48 +36,7 @@ prepare_for_plots_poly <- function(mod){
   return(dat)
 }
 
-#' Utility function to make a dataframe from the continuous IRTmodel
-#'
-#' This is a utility function to make a dataframe from the continuous IRTmodel, which makes it easier to plot the surfaces
-#'
-#' @param mod IRT model, either from function \code{cirtmodel} or the \code{R} package \code{EstCRM}.
-#' @param thetarange The range for \code{theta}, default from -6 to 6.
-#'
-#' @return Dataframe with output probabilities from the IRT model for all algorithms.
-#'
-#' @examples
-#' data(classification_cts)
-#' mod <- cirtmodel(classification_cts)
-#' dat <- prepare_for_plots_crm(mod$model)
-#' head(dat)
-#'
-#' @export
-#' @export
-prepare_for_plots_crm <- function(mod, thetarange = c(-6,6)){
-  num_algos <- dim(mod$data)[2]
-  names_algos <- colnames(mod$data)
-  ori_data <- mod$data
-  theta <- seq(thetarange[1], thetarange[2], by=0.1)
-  z <- seq(-6, 6, by=0.1)
-  theta_z <- expand.grid(theta, z)
-  colnames(theta_z) <- c("theta", "z")
 
-  for(i in 1:num_algos){
-    Algorithm <- rownames(mod$param)[i]
-    alpha <- mod$param[i, 1]
-    beta <- mod$param[i, 2]
-    gamma <- mod$param[i, 3]
-    pdf <- alpha*gamma/sqrt(2*pi)*exp(-alpha^2/2*(theta_z[ ,1]-beta-gamma*theta_z[ ,2])^2)
-    df <- cbind.data.frame(theta_z, pdf, rep(Algorithm, dim(theta_z)[1]))
-    colnames(df)[4] <- "Algorithm"
-    if(i==1){
-      gdf <- df
-    }else{
-      gdf <- rbind.data.frame(gdf, df)
-    }
-  }
-  return(gdf)
-}
 
 get_trace <- function(mod, num=1){
   extr <- mirt::extract.item(mod, num)
