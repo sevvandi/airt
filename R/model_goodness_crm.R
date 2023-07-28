b874a9ae00347e7626bafa31d6391d88745b0ea0
sevvandi <sevvandi.kandanaarachchi@gmail.com>
2023-07-28 15:50:35 +1000

    add autoplot for model goodness crm

--- R/model_goodness_crm.R
+++ R/model_goodness_crm.R
@@ -47,9 +47,14 @@ model_goodness_for_algo_crm <- function(mod, num=1){
 }
 
 
-actual_vs_predicted_crm <- function(mod, num=1, min_item=0, max_item=1){
+actual_vs_predicted_crm <- function(mod, num=1){
   # actual values
-  dat <- mod$data
+  dat <- as.data.frame(mod$data)
+
+  max.item <- apply(dat, 2, max)
+  min.item <- apply(dat, 2, min)
+  max_item <- max.item[num]
+
   algo_vals <- dat[ ,num]
   act_prd <- matrix(0, ncol=2, nrow=length(algo_vals))
   act_prd[ ,1] <- algo_vals
@@ -58,18 +63,12 @@ actual_vs_predicted_crm <- function(mod, num=1, min_item=0, max_item=1){
   alpha <- paras[num, 1]
   beta <- paras[num, 2]
   gamma <- paras[num, 3]
-  min.item <- rep(min_item, dim(dat)[2])
-  max.item <- rep(max_item, dim(dat)[2])
 
-  persons <- EstCRM::EstCRMperson(dat,paras,min.item,max.item)
+  persons <- EstCRM::EstCRMperson(dat,paras, min.item, max.item)
   scores <- persons$thetas[ ,2]
 
   z_vals <- log(algo_vals/(max_item - algo_vals))
   theta <- seq(from = min(scores), to=max(scores), by=0.01)
-  # z <- seq(from = min(z_vals), to=max(z_vals), by=0.01)
-  # z <- seq(from = -6, to=6, by=0.01)
-  # theta_z <- expand.grid(theta, z)
-  # colnames(theta_z) <- c("theta", "z")
 
   pdf2 <- (scores - beta)/gamma
   indsbig <- which(pdf2 > 10)
@@ -77,16 +76,6 @@ actual_vs_predicted_crm <- function(mod, num=1, min_item=0, max_item=1){
     pdf2[indsbig] <- 10
   }
   act_prd[ ,2] <- max_item*exp(pdf2)/(1 + exp(pdf2))
-  # pdf <- alpha*gamma/sqrt(2*pi)*exp(-alpha^2/2*(theta_z[ ,1]-beta-gamma*theta_z[ ,2])^2)
-  # pdfdf <- cbind.data.frame(theta_z, pdf)
-  # for(i in 1:length(scores)){
-  #   ind1 <- which.min(abs(pdfdf[ ,1] - scores[i]))
-  #   min_val <- pdfdf[ind1, 1]
-  #   inds <- which(pdfdf[ ,1] == min_val)
-  #   df2 <- pdfdf[inds, ]
-  #   val <- df2[which.max(df2[, 3]) ,2]
-  #   act_prd[i, 2] <- max_item*exp(val)/(1 + exp(val))
-  # }
 
   return(act_prd)
 }
@@ -97,7 +86,9 @@ actual_vs_predicted_crm <- function(mod, num=1, min_item=0, max_item=1){
 #'
 #' This function computes the goodness of the IRT model for all algorithms for different goodness tolerances.
 #'
-#' @inheritParams model_goodness_for_algo_crm
+#' @param model The output of function cirtmodel.
+#' @param object For autoplot: The output of model_goodness_crm.
+#' @param ...  Other arguments currently ignored.
 #'
 #' @return  A list with the following components:
 #' \item{\code{goodnessAUC}}{The area under the model goodness curve for each algorithm. }
@@ -105,18 +96,18 @@ actual_vs_predicted_crm <- function(mod, num=1, min_item=0, max_item=1){
 #' \item{\code{residuals}}{The residuals for each algorithm using the AIRT model.}
 #'@examples \donttest{
 #'set.seed(1)
-#'x1 <- runif(100)
-#'x2 <- runif(100)
-#'x3 <- runif(100)
+#'x1 <- runif(200)
+#'x2 <- 2*x1 + rnorm(200, mean=0, sd=0.1)
+#'x3 <- 1 - x1 + rnorm(200, mean=0, sd=0.1)
 #'X <- cbind.data.frame(x1, x2, x3)
-#'max_item <- rep(1,3)
-#'min_item <- rep(0,3)
-#'mod <- cirtmodel(X, max.item=max_item, min.item=min_item)
-#'out <- model_goodness_crm(mod$model)
+#'mod <- cirtmodel(X)
+#'out <- model_goodness_crm(mod)
 #'out
+#'autoplot(out)
 #'}
 #' @export
-model_goodness_crm <- function(mod){
+model_goodness_crm <- function(model){
+  mod <- model$model
   dd <- dim(mod$data)[2]
   nn <- dim(mod$data)[1]
   mse <- acc <- matrix(0, ncol=1, nrow=dd)
@@ -135,10 +126,32 @@ model_goodness_crm <- function(mod){
   colnames(curves) <- c("x", rownames(mod$param))
   rownames(acc) <-rownames(mod$param)
   colnames(residuals) <- rownames(mod$param)
-  out <- list()
-  out$goodnessAUC <- acc
-  out$mse <- mse
-  out$curves <- curves
-  out$residuals <- residuals
-  return(out)
+
+  structure(list(
+    goodnessAUC = acc,
+    mse = mse,
+    curves = curves,
+    residuals = residuals,
+    call = match.call()
+  ), class='modelgoodnesscrm')
+}
+
+
+#' @rdname model_goodness_crm
+#' @export
+autoplot.modelgoodnesscrm <- function(object,
+                                      ...){
+  Algorithm <- x <- value <- NULL
+
+  num_algos <- NROW(object$goodnessAUC)
+  colrs2 <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(8, "Dark2"))(num_algos)
+  good_curves <- as.data.frame(object$curves)
+  good_df <- good_curves %>%
+    tidyr::pivot_longer(cols=2:dim(good_curves)[2], names_to=c("Algorithm"))
+  ggplot(good_df, aes(x,value))  +
+    geom_line(aes(color = Algorithm), size=1)  +
+    ylab("CDF") +
+    xlab("Scaled Absolute Error") +
+    theme_bw() +
+    scale_color_manual(values = colrs2)
 }
